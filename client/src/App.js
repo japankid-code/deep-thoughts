2aed62a7a2947ad46000c465ef8a3565cc08497a
japankid-code <japankid.jake@gmail.com>
2021-07-10 14:52:42 -0500

    add backend connection, thoughtlist and

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
+
+import {
+  ApolloProvider, // provides data to componenents
+  ApolloClient, // constructor, initializes connection to server
+  InMemoryCache, // caches API responses for efficiency
+  createHttpLink, // controls how client makes requests
+} from "@apollo/client";
+
+const httpLink = createHttpLink({
+  uri: "/graphql", // links to GraphQL server
+});
+
+const client = new ApolloClient({
+  link: httpLink, // links to API endpoint
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
